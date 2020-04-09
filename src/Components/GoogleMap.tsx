c7e07c68c24bbd4042eb3743be00d556044f4777
yuya.miyamoto <yuya.miyamoto@uniadex.co.jp>
2020-04-09 16:00:30 +0900

    Iframeの属性を削除

--- src/Components/GoogleMap.tsx
+++ src/Components/GoogleMap.tsx
@@ -3,7 +3,7 @@ import Iframe from 'react-iframe'
 
 const GoogleMap = () => {
   return (
-    <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2403003.6041441546!2d133.3725553385395!3d33.71642423542033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35536ef3f5fdd441%3A0xb610aad0dc85ef54!2z5b6z5bO255yM5p2_6YeO6YOh5p2-6IyC55S6!5e0!3m2!1sja!2sjp!4v1586407643133!5m2!1sja!2sjp" width="400" height="300" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></Iframe>
+    <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2403003.6041441546!2d133.3725553385395!3d33.71642423542033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35536ef3f5fdd441%3A0xb610aad0dc85ef54!2z5b6z5bO255yM5p2_6YeO6YOh5p2-6IyC55S6!5e0!3m2!1sja!2sjp!4v1586407643133!5m2!1sja!2sjp"></Iframe>
   )
 };
 
